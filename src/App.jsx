c4f5556baa0daab8496cf71916f904bd9ebd5c75
Jerome <jeromeb4u@gmail.com>
2025-09-24 21:18:42 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Header from "./components/Header.jsx";
+import Player from "./components/Player.jsx";
+function App() {
+  return (
+    <>
+      <Header />
+      {/* <p>Coming soon...</p> */}
+      <main>
+        <div id="game-container">
+          <ol id="players">
+            <Player name="Player 1" symbol="X" />
+            <Player name="Player 2" symbol="O" />
+          </ol>
+          Game Board
+        </div>
+        LOG
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header() {
+    return (
+        <>
+        <header>
+
+            <img src="public\game-logo.png" alt="Hand drawn tic-tac-toe image" />
+            <h1>React Tic-Tac-Toe</h1>
+        </header>
+        </>
+    )
+}
